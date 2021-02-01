fcdc09c4b642d7827fb9004b28e5793be5f41771
leesangdoc <lumibres@gmail.com>
2021-02-02 00:35:13 +0900

    .

--- src/components/QuickInterestStockRight.vue
+++ src/components/QuickInterestStockRight.vue
@@ -43,6 +43,51 @@ export default {
       },
        
       chartOptions: {
+        rangeSelector: {
+          allButtonsEnabled: true,
+          verticalAlign: "top",
+          floating: true,
+          y: 0,
+          x: 0,
+          // buttons: [{
+          //       type: 'month',
+          //       count: 3,
+          //       text: 'Day',
+          //       dataGrouping: {
+          //           forced: true,
+          //           units: [['day', [1]]]
+          //       }
+          //   }, {
+          //       type: 'year',
+          //       count: 1,
+          //       text: 'Week',
+          //       dataGrouping: {
+          //           forced: true,
+          //           units: [['week', [1]]]
+          //       }
+          //   }, {
+          //       type: 'all',
+          //       text: 'Month',
+          //       dataGrouping: {
+          //           forced: true,
+          //           units: [['month', [1]]]
+          //       }
+          //   }],
+            buttonTheme: {
+                width: 60
+            },
+            selected: 1
+        },
+        xAxis: {
+          type: "category",
+          title: {
+            text: 'Date'
+          },
+          labels: {
+            autoRotationLimit: 40
+          },
+          
+        },
         chart: { 
           renderTo: 'container',
           type: 'line',
@@ -54,17 +99,7 @@ export default {
         subtitle: {
           text: 'Left side of the text should be aligned to the tick'
         },
-        xAxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          type: 'datetime',
-          title: {
-            text: 'Date'
-          },
-          labels: {
-            autoRotationLimit: 40
-          },
-          
-        },
+        
         tooltip: {
           crosshairs: true,
           shared: true
@@ -76,16 +111,63 @@ export default {
           title: {
               text: 'Fruit eaten'
           },
+          alignTicks:'left', 
+          textAlign:'left',
+          align:'middle',
+          opposite:false,
           labels: {
             formatter: function () {
                 return this.value;
             }
           }
         },
-
+        responsive: {
+          rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                chart: {
+                    height: 300
+                },
+                subtitle: {
+                    text: null
+                },
+                navigator: {
+                    enabled: true
+                },
+                legend: {
+                  enabled: false
+                },
+                yAxis: {
+                  title: {
+                    enabled: false
+                  }
+                }
+            }
+          }]
+        },
         series: [{
           name: '준봉',
-          data: [1,2,3,4,5,6,7,8,10,11,12,13] // sample data
+          pointStart: Date.UTC(2018, 1, 1),
+          pointInterval: 1000 * 3600 * 24,
+          data: [1,2,3,4,5,6,7,8,10,11,12,13,77,8,65,4,2,4,5,7,88], // sample data
+          marker: {
+            enabled: null, // auto
+            radius: 3,
+            lineWidth: 1,
+            lineColor: '#FFFFFF'
+          },
+          tooltip: {
+              valueDecimals: 0
+          },
+          dataGrouping:{
+            approximation: 'average',
+            enabled:true,
+            forced:true,
+            groupAll:true,
+            groupPixelWidth:15
+          }
         }]
       }
   }),
